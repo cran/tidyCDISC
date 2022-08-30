9e230be4ced6c630b0c9456c7d76f97d59a27164
Aaron Clark <clark.aaronchris@gmail.com>
2022-08-30 12:30:02 +0000

    version 0.1.0

---
+++ inst/doc/getting_started.R
@@ -0,0 +1,51 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ---- echo=FALSE, fig.align='center'------------------------------------------
+knitr::include_graphics("cran_figures/TG_need_help.PNG")
+
+## ---- out.width="95%", fig.align = 'center', echo=FALSE-----------------------
+knitr::include_graphics("cran_figures/landing_page_tidyCDISC.PNG")
+
+## ---- echo=FALSE, out.width="95%", fig.align = 'center'-----------------------
+knitr::include_graphics("cran_figures/quick_upload_then_tg_tidyCDISC_red.PNG")
+
+## ---- echo=FALSE, out.width="95%", fig.align = 'center'-----------------------
+knitr::include_graphics("cran_figures/02-TG-tidyCDISC.PNG")
+
+## ---- out.width="80%", echo=FALSE, fig.align='center'-------------------------
+knitr::include_graphics("cran_figures/bins.PNG")
+
+## ---- echo=FALSE, out.width="95%", fig.align = 'center'-----------------------
+knitr::include_graphics("cran_figures/03-age_mean.PNG")
+
+## ---- echo=FALSE, out.width="95%", fig.align = 'center'-----------------------
+knitr::include_graphics("cran_figures/04-diabp_mean.PNG")
+
+## ---- echo=FALSE, out.width="85%", fig.align = 'center'-----------------------
+knitr::include_graphics("cran_figures/TG_STAN_dropdown.PNG")
+
+## ----  echo=FALSE, out.width="95%", fig.align = 'center'----------------------
+knitr::include_graphics("cran_figures/t_disp_red.PNG")
+
+## ----  echo=FALSE, out.width="95%", fig.align = 'center'----------------------
+knitr::include_graphics("cran_figures/line_plot_mean_over_time_tidyCDISC.PNG")
+
+## ----  echo=FALSE, out.width="95%", fig.align = 'center'----------------------
+knitr::include_graphics("cran_figures/line_plot_mean_over_time_breakdown.PNG")
+
+## ----  echo=FALSE, out.width="50%", fig.align = 'center'----------------------
+knitr::include_graphics("cran_figures/filter_patients.PNG")
+
+## ----  echo=FALSE, out.width="95%", fig.align = 'center'----------------------
+knitr::include_graphics("cran_figures/drill_down_events.PNG")
+
+## ---- echo=FALSE, out.width="95%", fig.align = 'center'-----------------------
+knitr::include_graphics("cran_figures/drill_down_visits.PNG")
+
+## ----  echo=FALSE, out.width="50%", fig.align = 'center'----------------------
+knitr::include_graphics("cran_figures/download_report.PNG")
+
